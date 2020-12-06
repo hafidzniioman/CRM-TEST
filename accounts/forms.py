f308f1e9abec7830f81378923c73482259c3a1a8
hafidzniioman <hafidzniioman@gmail.com>
2020-12-07 00:24:43 +0700

    change db to mysql

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,8 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
-from .models import Order
+
+from .models import *
 
 
 class OrderForm(ModelForm):
@@ -12,6 +12,6 @@ class OrderForm(ModelForm):
 
 
 class CreateUserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
