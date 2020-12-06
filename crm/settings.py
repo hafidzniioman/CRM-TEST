98b747793d3346b2479d34d6a9fcd39bbeb54cbc
hafidzniioman <hafidzniioman@gmail.com>
2020-12-06 12:32:13 +0700

    error postgresql connection

--- crm/settings.py
+++ crm/settings.py
@@ -79,9 +79,12 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
-        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tulustriantoro',
+        'USER': 'tulustriantoro',
+        'PASSWORD': '201402',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
